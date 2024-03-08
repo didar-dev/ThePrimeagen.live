dc75fd219e228f68978c21f1136b94e7e1998496
Didar Abdulkhaliq <didar0xalil@gmail.com>
2024-03-08 22:53:02 +0300

    add teej

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,33 +7,13 @@ export default function Home() {
       className="flex min-h-screen max-h-screen flex-col lg:flex-row items-center gap-4 relative"
     >
       <div className="w-1/2 flex flex-col items-center justify-center gap-4">
-        <Image
-          src="/hero.webp"
-          alt="The Primeagen"
-          width={800}
-          height={700}
-          priority={true}
-          style={{
-            userSelect: "none",
-          }}
-          draggable={false}
-          className="block lg:hidden absolute bottom-0 right-0"
-        />
-        <div className="w-screen h-[300px] md:w-[720px] md:h-[478px]">
+        <div className="w-screen h-[300px] md:w-[720px] md:h-[478px] mb-[250px]">
           <iframe
             src="https://player.twitch.tv/?channel=theprimeagen&parent=www.theprimeagen.live"
             title="ThePrimeagen"
             width="100%"
             height="100%"
           ></iframe>
-        </div>
-        <div className="flex gap-4 flex-col items-center justify-center">
-          <h1 className="text-3xl md:text-5xl text-white font-bold text-center">
-            ThePrimeagen
-          </h1>
-          <p className="text-lg md:text-2xl text-white text-center">
-            A legend who works at Netflix and streams on Twitch.
-          </p>
         </div>
       </div>
       <div className="hidden lg:block w-1/2 min-h-screen relative">
@@ -48,6 +28,30 @@ export default function Home() {
         />
         <div className="absolute hidden lg:block inset-0 bg-gradient-to-r from-black to-transparent"></div>
       </div>
+      <Image
+        src="/teej.png"
+        alt="The Primeagen"
+        width={400}
+        height={400}
+        priority={true}
+        style={{
+          userSelect: "none",
+        }}
+        draggable={false}
+        className="absolute bottom-0 left-0 w-[270px] lg:w-[400px] object-contain"
+      />
+      <Image
+        src="/hero.webp"
+        alt="The Primeagen"
+        width={800}
+        height={700}
+        priority={true}
+        style={{
+          userSelect: "none",
+        }}
+        draggable={false}
+        className="absolute bottom-0 right-0 w-[270px] lg:w-[400px] object-contain"
+      />
     </main>
   );
 }
